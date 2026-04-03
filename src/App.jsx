d5056a86a11aabda32965165f0741b0c9c587050
Travo1ta <vladimirkobec@gmail.com>
2026-04-03 13:58:31 +0300

    Fixing the scroll

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { AppRoute } from "./const";
 import PageWrapper from "./components/layout/page-wrapper/page-wrapper";
 import MainPage from "./components/pages/main-page";
 import Order from "./components/pages/order/order";
+import ScrollToTop from "./components/ui/scroll-top/scroll-top";
 import features from "./mocks/features";
 import products from "./mocks/products";
 
@@ -13,10 +14,11 @@ function App() {
       <>
          <GlobalStyle />
          <Router>
+            <ScrollToTop />
             <Routes>
                <Route path={AppRoute.MAIN} element={<PageWrapper />}>
                   <Route index element={<MainPage features={features} />} />
-                  <Route path={AppRoute.ORDER.replace(AppRoute.MAIN, "")} element={<Order products={products} />} />
+                  <Route path={AppRoute.ORDER} element={<Order products={products} />} />
                </Route>
             </Routes>
          </Router>
